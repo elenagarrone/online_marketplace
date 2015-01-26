cbb73ba8dbf7b2b8a52dce71872fea87f4530879
Elena <elena.15@hotmail.it>
2015-01-26 16:20:48 +0000

    modified titles of test that where not appropriate

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -36,32 +36,32 @@ describe Checkout do
     expect(co.basket).to eq([])
   end
 
-  it 'should caluclate the total of the basket' do
+  it 'should caluclate the total of the basket if no promotion applies' do
     co.scan(lavender_heart)
     co.scan(kids_tshirt)
     expect(co.total).to eq(29.2)
   end
 
-  it 'should caluclate the total of the basket with the promotions ON ITEM' do
+  it 'should caluclate the total of the basket with the promotions' do
     2.times { co.scan(lavender_heart) }
     expect(co.pre_total).to eq(17.0)
   end
 
-  it '(EXAMPLE 1) should caluclate the total of the basket with the promotions ON TOTAL' do
+  it '(Basket: 001,002,003) should caluclate the total of the basket with the promotions' do
     co.scan(lavender_heart)
     co.scan(personalised_cufflinks)
     co.scan(kids_tshirt)
     expect(co.total).to eq(66.78)
   end
 
-  it '(EXAMPLE 2) should caluclate the total of the basket with the promotions' do
+  it '(Basket: 001,003,001) should caluclate the total of the basket with the promotions' do
     co.scan(lavender_heart)
     co.scan(kids_tshirt)
     co.scan(lavender_heart)
     expect(co.total).to eq(36.95)
   end
 
-  it '(EXAMPLE 3) should caluclate the total of the basket with the promotions' do
+  it '(Basket: 001,002,001,003) should caluclate the total of the basket with the promotions' do
     co.scan(lavender_heart)
     co.scan(personalised_cufflinks)
     co.scan(lavender_heart)
@@ -69,35 +69,39 @@ describe Checkout do
     expect(co.total).to eq(73.76)
   end
 
-  it '1. should caluclate the total of the basket with the promotions (checking different promotions)' do
-    four_or_more = X_or_more_items.new(4, kids_tshirt, 8.50)
-    promotional.add(four_or_more)
-    4.times { co.scan(lavender_heart) }
-    4.times { co.scan(kids_tshirt) }
-    expect(co.total).to eq(61.20)
-  end
+  context 'Checking behaviour of total when:' do
 
-  it '2. should caluclate the total of the basket with the promotions (checking different promotions)' do
-    tewnty_percent_off = Percent_off_total.new(20, 100.0)
-    promotional.add(tewnty_percent_off)
-    forty_percent_off = Percent_off_total.new(40, 130.0)
-    promotional.add(forty_percent_off)
-    3.times { co.scan(personalised_cufflinks) }
-    expect(co.total).to eq(81.00)
-  end
+    it 'two promotions on items are present' do
+      four_or_more = X_or_more_items.new(4, kids_tshirt, 8.50)
+      promotional.add(four_or_more)
+      4.times { co.scan(lavender_heart) }
+      4.times { co.scan(kids_tshirt) }
+      expect(co.total).to eq(61.20)
+    end
 
-  it '3. should caluclate the total of the basket with the promotions (checking different promotions)' do
-    forty_percent_off = Percent_off_total.new(40, 200.0)
-    promotional.add(forty_percent_off)
-    3.times { co.scan(personalised_cufflinks) }
-    expect(co.total).to eq(121.50)
-  end
+    it 'three promotions on total are present' do
+      tewnty_percent_off = Percent_off_total.new(20, 100.0)
+      promotional.add(tewnty_percent_off)
+      forty_percent_off = Percent_off_total.new(40, 130.0)
+      promotional.add(forty_percent_off)
+      3.times { co.scan(personalised_cufflinks) }
+      expect(co.total).to eq(81.00)
+    end
 
-  it '4. should caluclate the total of the basket with the promotions (checking different promotions)' do
-    five_percent_off = Percent_off_total.new(5, 61.0)
-    promotional.add(five_percent_off)
-    3.times { co.scan(personalised_cufflinks) }
-    expect(co.total).to eq(128.25)
+    it "two promotions on total are present but one is not applicable" do
+      forty_percent_off = Percent_off_total.new(40, 200.0)
+      promotional.add(forty_percent_off)
+      3.times { co.scan(personalised_cufflinks) }
+      expect(co.total).to eq(121.50)
+    end
+
+    it "two promotions are present (one with rule.amount bigger and percentage smaller than the other)" do
+      five_percent_off = Percent_off_total.new(5, 61.0)
+      promotional.add(five_percent_off)
+      3.times { co.scan(personalised_cufflinks) }
+      expect(co.total).to eq(128.25)
+    end
+
   end
 
 end
